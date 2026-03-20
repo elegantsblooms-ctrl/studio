84be5c8ae5c6c543c3ece011befc128efc1addc3
Elegants Blooms <elegantsblooms@gmail.com>
2026-03-20 18:17:20 +0000

    Name - pink rose Price - 350 https://res.cloudinary.com/dh9mpwpxx/image

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -8,6 +9,7 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 const products = [
   { id: 1, name: "Rose Romance Bouquet", price: "₹349", time: "5-7 days", tag: "Bestseller", imgId: "bouquet-1" },
   { id: 7, name: "Violet Rose", price: "₹350", time: "5-7 days", tag: "New", imgId: "violet-rose" },
+  { id: 11, name: "Pink Rose", price: "₹350", time: "5-7 days", tag: "New", imgId: "pink-rose-cloudinary" },
   { id: 9, name: "Red + Pink", price: "₹350", time: "5-7 days", tag: "New", imgId: "red-pink" },
   { id: 10, name: "Munchy Violet", price: "₹400", time: "5-7 days", tag: "New", imgId: "munchy-violet" },
   { id: 8, name: "Pink + Reddish Pink", price: "₹400", time: "5-7 days", tag: "New", imgId: "pink-reddish-pink" },
